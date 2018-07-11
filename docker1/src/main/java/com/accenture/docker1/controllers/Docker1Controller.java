d0910cac304fe85314bc11a8be9ab7c4b3fc8ae1
Abdool, Mariam N <mariam.n.abdool@accenture.com>
2018-07-11 14:40:29 +0400

    First commit

--- docker1/src/main/java/com/accenture/docker1/controllers/Docker1Controller.java
+++ docker1/src/main/java/com/accenture/docker1/controllers/Docker1Controller.java
@@ -1,22 +1,23 @@
 package com.accenture.docker1.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/")
 public class Docker1Controller {
 	
 	@GetMapping("/")
 	public String hello() {
-		return "hello";
+	return "hello";
 	}
-	
-	@PostMapping("/")
-	public String sayHello() {
-		return "hello";
+
+	@PostMapping("/user")
+	public String helloSomeone(@RequestParam String user, Model model) {
+	model.addAttribute("user", user);
+	return "hello";
 	}
 
 }
